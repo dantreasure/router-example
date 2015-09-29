432dda2ae66b8924dd741aba4d78f3aa72b9bda2
Daniel Treasure <dbtreasure@gmail.com>
2015-09-28 20:54:44 -0600

    adds a factory to fetch icelandic earthquakes and injects this factory on dimension1 controller

--- dimension1/controller.js
+++ dimension1/controller.js
@@ -1,3 +1,9 @@
-app.controller('dim1ctrl', ['$scope', function($scope){
+'use strict';
+
+app.controller('dim1ctrl', ['$scope', 'earthquakes', function($scope, earthquakes){
 	$scope.message = 'Welcome to Dimension 1!';
-}])
+
+	earthquakes.get().then(function(data){
+		$scope.earthquakes = data.data.results;
+	})
+}]);
